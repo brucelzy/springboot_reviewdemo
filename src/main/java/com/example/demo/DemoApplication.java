117baf8979afe29e5fa4a742aee3e9fb0b4c6164
brucelzy <614783664@qq.com>
2020-06-08 23:05:00 +0800

    提交人：李中玉 提交原因：修改代码

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//这是我的第一次修改
 @SpringBootApplication
 public class DemoApplication {
 
